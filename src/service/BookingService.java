aee93c2ce15c5386fac99a00ed28f3ccd8efe559
dth27 <Dottedsocks@htgi-dw0143.rhi.hi.is>
2017-04-18 13:30:38 +0000

    Second Commit

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -52,60 +52,55 @@ public class BookingService {
          GretaTheDBManager.createBooking(bookingno, flightId, numbofPass); 
      }
      
-     //TODO kallar a Databasemanager og naer i upls ur gagnagrunni
-     //Ekki búin að gera þetta ennþá, var ekki alveg viss hvernig þetta kæmi frá gangagrunninum
+     //TODO
      public ArrayList getFlight(int bookingNo){
          return null;
      }
-     //TODO væri ekki best ef þetta tæki inn "int bookingNo" og fyndi alla Passenger sem væru með þetta bookingNo?
-     //Ekki búin að gera þetta ennþá, var ekki alveg viss hvernig þetta kæmi frá gangagrunninum
+     //TODO
      public ArrayList getPassenger(int bookingNo){
          return null;
      }
-     //sleppa nema timi gefst
-    public void removePassenger(int passenger){
-        
-    }
-     //sleppa nema timi gefst   
+
+     //TODO 
      public boolean updatePayment(){
         return true;
     }
-     //TODO SQL skipun i databasemanager sem naer i MAX bookingno 
+     
      //Finds the maximum booking no in the database and adds one to that number to create a new booking no.
     public int getBookingNo(){
         int i = GretaTheDBManager.getMaxBookingNo();
         return i+1;
     }
- 
-    //TODO þetta virkar ekki rétt þ.e. passInfo virkar ekki, svo ég er að gera þetta eitthvað vitlaust
+
     //Takes in an Array of PassengerInfo made by calling the method getPassenger with the relevant bookingNo
     //And creates a TableModel containing the array of Passengers   
-   /*public DefaultTableModel bookingPassTable(passInfo = getPassenger(int bookingNo)){
+   public DefaultTableModel bookingPassTable(){
         //TODO na i upplysingar um passenger og setja i Tablemodel og skila
         //getPassenger
         String col[] = {"name","SSno","bookingNo"};
         DefaultTableModel Tablemodel = new DefaultTableModel(col, 0);
         
-        for (int i = 0; i< passInfo.size(); i++){
+        /*for (int i = 0; i< passInfo.size(); i++){
             String passName = passInfo.get(i).getName();
             String passSSno = passInfo.get(i).getSSno();
             Object[] data = {passName, passSSno};
              Tablemodel.addRow(data);
-        }
+        }*/
         return Tablemodel;
-    } 
+   }
+
     
      //TODO þetta virkar ekki rétt þ.e. flightInfo virkar ekki, svo ég er að gera þetta eitthvað vitlaust
     //Takes in an Array of Flight made by alling the method getFlight with the relevant bookingNo
     //And creates a TableModel containing the array of Passengers   
-    public DefaultTableModel bookingFlightTable(flightInfo = getFlight(int bookingNo)){
+    public DefaultTableModel bookingFlightTable(){
         //TODO na i upplysingar um flug og setja i tablemodel og skila
         //getFlight
         
         String col[] = {"arrivingTime","airline","departingTime", "date", "price", "destination", "departingfrom", "flight number"};
         DefaultTableModel Tablemodel = new DefaultTableModel(col, 0);
         
-        for (int i = 0; i< flightInfo.size(); i++){
+      /*  for (int i = 0; i< flightInfo.size(); i++){
             int arrTime = flightInfo.get(i).getArrival_time();
             String airline = flightInfo.get(i).getAirline();
             int depTime = flightInfo.get(i).getDeparture_time();
@@ -117,7 +112,7 @@ public class BookingService {
             Object[] data = {arrTime, airline, depTime, FlDate, ticketPrice, destination, 
                                departingFrom, flightNO};
              Tablemodel.addRow(data);
-        }
+        }*/
         return Tablemodel;
-    }*/
+    }
 }
